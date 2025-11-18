6f18657225d38c4e582cab44e033a2a1432d757d
Thomas Shellberg <6723003+tommyshellberg@users.noreply.github.com>
2025-11-18 14:56:04 +0100

    Fix missing liveactivities target and code

---
+++ ios/liveactivities/liveactivitiesBundle.swift
@@ -0,0 +1,16 @@
+//
+//  liveactivitiesBundle.swift
+//  liveactivities
+//
+//  Created by thomas shellberg on 23.04.25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct liveactivitiesBundle: WidgetBundle {
+    var body: some Widget {
+        liveactivitiesLiveActivity()
+    }
+}
